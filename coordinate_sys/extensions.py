fe3aa57fac5699c1d2bd40bb05c420bde53dbe8b
angelyhch <angelyhch@163.com>
2020-11-20 17:20:45 +0800

    增加Dbobj class 用于操作数据库。

--- coordinate_sys/extensions.py
+++ coordinate_sys/extensions.py
@@ -5,7 +5,8 @@ from flask_mail import Mail
 from flask_caching import Cache
 import os
 from sqlalchemy import create_engine, inspect
-from coordinate_sys.settings import DevelopmentConfig
+import pandas as pd
+
 
 toolbar = DebugToolbarExtension()
 db = SQLAlchemy()
@@ -19,3 +20,33 @@ root_path = os.path.normcase(os.path.dirname(__file__))
 engine = create_engine('mysql+pymysql://coordinate:coordinate_data@127.0.0.1:3306/coordinate_data')
 # engine = create_engine(DevelopmentConfig.SQLALCHEMY_DATABASE_URI)  #todo: 利用env未成功
 db_inspector = inspect(engine)
+
+
+class Dbobj:
+    '''
+    数据库连接实例
+    engine 是 create_engine返回对象。
+    '''
+    def __init__(self, engine):
+        self.engine = engine
+
+    def excel_to_table(self, read_excel_name, to_table_name, temp_folder=r"static\process_data_temp", to_schema='coordinate_data', header=0, index_col=None):
+        # 设定读取数据源地址
+        excel_path = os.path.join(root_path, temp_folder, read_excel_name)
+        # 读取EXCEL数据
+        df1 = pd.read_excel(excel_path, header=header, index_col=index_col)
+        # EXCEL数据写入数据库
+        df1.to_sql(to_table_name, self.engine, schema=to_schema, if_exists='replace')  # todo: 以后要和三坐标分析拆分数据库或者拆表
+
+    def read_table(self, table_name, index_col=None):
+        df_read = pd.read_sql_table(table_name, self.engine, index_col=index_col)
+        return df_read
+#
+# def manual_input_data(read_excel_name, to_table_name, engine, temp_folder=r"static\process_data_temp", to_schema='coordinate_data', header=0, index_col=None):
+#     # 设定读取数据源地址
+#     excel_path = os.path.join(root_path, temp_folder, read_excel_name)
+#     # 读取EXCEL数据
+#     df1 = pd.read_excel(excel_path, header=header, index_col=index_col)
+#     # EXCEL数据写入数据库
+#     df1.to_sql(to_table_name, engine, schema=to_schema, if_exists='replace') #todo: 以后要和三坐标分析拆分数据库或者拆表
+
--- coordinate_sys/process_model.py
+++ coordinate_sys/process_model.py
@@ -1,21 +1,8 @@
-from coordinate_sys.models import engine
-from coordinate_sys.extensions import db
+from coordinate_sys.extensions import engine, Dbobj
 import pandas as pd
 from coordinate_sys import root_path
 import os, sys
 
-class Station(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(128))
-    line = db.Column(db.String(128))
+dbo = Dbobj(engine)
 
-
-def manual_input_data_to_station():
-    # 设定读取数据源地址
-    excel_path = os.path.join(root_path, r"static\process_data_temp", r"station_1108.xlsx")
-    # 读取EXCEL数据
-    df1 = pd.read_excel(excel_path, header=0, index_col=None)
-    # EXCEL数据写入数据库
-    df1.to_sql('station', engine, schema='coordinate_data', if_exists='replace') #todo: 以后要和三坐标分析拆分数据库或者拆表
-
+dbo.excel_to_table('station_1108.xlsx', 'temp_station')
